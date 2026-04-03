0b0ebe4a9a46abb663e273979f04cfd12dea9f69
Jacob Ebey <jacob.ebey@live.com>
2026-04-03 00:54:01 -0700

    cleanup

--- apps/vite/src/actions/auth.ts
+++ apps/vite/src/actions/auth.ts
@@ -21,7 +21,7 @@ export async function logoutAction(): Promise<LogoutResult> {
     redirect(routes.frames.home.href());
     return {
       success: true,
-      message: "Logged out successfully.",
+      message: "Redirecting...",
     };
   } catch (reason) {
     console.error(reason);
